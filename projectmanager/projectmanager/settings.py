2156dff75eaaec25a42d9c18763937d0e6427e54
ff <framef318@gmail.com>
2023-12-22 11:30:50 +0700

    db

--- projectmanager/projectmanager/settings.py
+++ projectmanager/projectmanager/settings.py
@@ -132,3 +132,5 @@ MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'userprofile.User'
--- projectmanager/userprofile/models.py
+++ projectmanager/userprofile/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+import uuid
 # Create your models here.
+class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    phone = models.CharField(max_length=10, blank=True)
